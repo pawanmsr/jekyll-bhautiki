c191009d06dcfcc2e4a13d127439fda898ae26ad
Pawan Mishra <pawanmsr@outlook.com>
2025-12-24 21:59:53 +0530

    scaffold note plugin

---
+++ _plugins/note.rb
@@ -0,0 +1,17 @@
+require ""
+
+# Runs before site generation.
+module Note
+    # Markup generator for notes.
+    class NoteGenerator < Jekyll::Generator
+        safe true
+        
+        # Collect posts.
+        # Find notes in post.
+        # Sort notes data.
+        # Reorganize data.
+        # Texture and presentation.
+        def generate(site)
+        end
+    end
+end
